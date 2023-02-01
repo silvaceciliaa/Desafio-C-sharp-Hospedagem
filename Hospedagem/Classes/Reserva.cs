18094f61754dca1703c46724f8e62aae349a68f6
Cecília Silva <ceciliaalicesilva88@gmail.com>
2023-02-01 00:11:57 -0300

    initial commit

---
+++ Hospedagem/Classes/Reserva.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Hospedagem.Classes
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (Suite.Capacidade >= hospedes.Count())
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Desculpe, não somos capazes de comportar este número de hóspedes");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count();
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            
+            
+            decimal valor = DiasReservados* Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados >= 10)
+            {
+                valor = valor - (valor / 10);
+            }
+
+            return valor;
+        }
+    }
+}
---
+++ Hospedagem/Classes/Suite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hospedagem.Classes
+{
+        public class Suite
+        {
+            public Suite() { }
+
+            public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+            {
+                TipoSuite = tipoSuite;
+                Capacidade = capacidade;
+                ValorDiaria = valorDiaria;
+            }
+
+            public string TipoSuite { get; set; }
+            public int Capacidade { get; set; }
+            public decimal ValorDiaria { get; set; }
+        }
+}
